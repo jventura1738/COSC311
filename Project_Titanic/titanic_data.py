d7362159b3dfc827b66457b392af57dee00651ce
Justin Ventura <justinv9001@aol.com>
2020-12-03 16:28:01 -0500

    Update

---
+++ Project_Titanic/titanic_data.py
@@ -0,0 +1,9 @@
+# Justin Ventura & Blaine Mason
+# Titanic Data Train-Test Module
+
+"""
+This module contains a bunch of useful functions to be used
+in the process of splitting the train/test data, prepare &
+clean data, and more.
+"""
+
