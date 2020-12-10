3f84827ecc9df49562ce21ba74c9663c0f345e6a
Justin Ventura <justinv9001@aol.com>
2020-12-09 20:38:50 -0500

    starting neural network

--- Project_Titanic/NeuralNetwork.py
+++ Project_Titanic/NeuralNetwork.py
@@ -9,9 +9,40 @@ survived the titanic sinking or not.
 
 import math
 import numpy as np
+from typing import Iterable
 
 
 # -----------------------------------------------------------------------------
+
+
+# Titanic Neural Network Vector Class.
+class TNN_Vector:
+
+    # Constructor
+    def __init__(self, data: Iterable = None) -> None:
+        """ Constructor for TNN_Vector Class.
+
+        Args:
+            data (Iterable, optional): Data for vector. Defaults to None.
+
+        Returns:
+            None.
+        """
+        self.data = np.array(data) if data is not None else None
+
+    # Static method for dot product.
+    @staticmethod
+    def dot(u, v) -> float:
+        """ Dot product.
+
+        Args:
+            u (TNN_Vector): vect 1.
+            v (TNN_Vector): vect 2.
+
+        Returns:
+            float: dot product result.
+        """
+        return sum([u_i * v_i for u_i, v_i in zip(u.data, v.data)])
 
 
 # Neural Network Class
@@ -19,8 +50,27 @@ class Tneural_network:
     """ Neural Network class for the titanic dataset. """
 
     # Constructor.
-    def __init__(self):
-        self.neurons = None
+    def __init__(self) -> None:
+        self.network = None
+
+    # Training method:
+    def train_network(self, vectors) -> None:
+        pass
+
+    # Feeding helping method.
+    def feed_forward(self, input_vector):
+        outputs = []
+
+        # Iterate layers of the neural network.
+        for layer in self.network:
+            input_with_bias = input_vector + [1]
+            output = [Tneural_network.neuron_output(neuron, input_with_bias)
+                      for neuron in layer]
+
+            outputs.append(output)
+
+        # Returns the output of each layer.
+        return np.array(outputs)
 
     # Static step function
     @staticmethod
@@ -31,6 +81,11 @@ class Tneural_network:
     @staticmethod
     def sigmoid(t: float) -> float:
         return 1 / (1 + math.exp(-t))
+
+    # Static neuron output function.
+    @staticmethod
+    def neuron_output(weights, inputs) -> float:
+        return Tneural_network.sigmoid(np.dot(weights, inputs))
 
 
 # -----------------------------------------------------------------------------
