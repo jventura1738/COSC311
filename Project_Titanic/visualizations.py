6b10735592b928dc19b0b9e944f94ae5225ff6ed
Justin Ventura <justinv9001@aol.com>
2020-12-15 01:14:23 -0500

    finish tomorrow

--- Project_Titanic/visualizations.py
+++ Project_Titanic/visualizations.py
@@ -13,3 +13,41 @@ FAIL = '\033[91m'
 ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
+
+# -----------------------------------------------------------------------------
+
+# TODO: make the visualizations for presentation.  These methods will perform
+# calculations and print the results to the screen.  The purpose is to minimize
+# the amount of code on the presentation notebook for the sake of organization.
+
+
+def get_age_distributions(titanic_dataset):
+    pass
+
+
+def get_pclass_distributions(titanic_dataset):
+    pass
+
+
+def get_sex_distributions(titanic_dataset):
+    pass
+
+
+def sex_vs_survival(titanic_dataset):
+    pass
+
+
+def pclass_vs_survival(titanic_dataset):
+    pass
+
+
+def age_vs_survival(titanic_dataset):
+    pass
+
+
+def adultmale_vs_womenandchildren(titanic_dataset):
+    pass
+
+
+def get_correlation_heatmap(titanic_dataset):
+    pass
